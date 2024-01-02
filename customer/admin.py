f0d47a66801da939cb4487e7ceaf1ceb394fe5e8
YASHWANT-HK <yashkgr2003@gmail.com>
2024-01-02 05:52:25 +0530

    customer database created

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+#from customer.models import customer
+from customer.models import customers_h
 
 # Register your models here.
+#admin.site.register(customer)
+
+admin.site.register(customers_h)
--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+#class customer(models.Model):
+    #address = models.CharField(max_length=255)
+    #ssword = models.CharField(max_length=255)
+
+    #def __str__(self):
+        #return self.email
+    
+class customers_h(models.Model):
+    address = models.CharField(max_length=255)
+    full_name = models.CharField(max_length=255)
+    Adharcard_no = models.IntegerField()
+    phone = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.email
